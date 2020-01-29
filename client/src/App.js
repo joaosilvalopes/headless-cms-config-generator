7670e474737a93ccf6c3fb5b595dda00effca464
joaosilvalopes <jpgdsl@gmail.com>
2020-01-29 13:20:36 +0000

    chore(client): Client project setup

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+
+function App() {
+	return (
+		<>
+			<CssBaseline />
+			<main />
+		</>
+	);
+}
+
+export default App;
