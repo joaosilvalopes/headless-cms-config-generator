9e7dacf1d0e9cfbc102a3808ae62706fae84aad5
joaosilvalopes <jpgdsl@gmail.com>
2020-01-31 01:42:12 +0000

    chore(server): Fix verify email success response

--- server/routes/post-verify-email.js
+++ server/routes/post-verify-email.js
@@ -1,24 +1,29 @@
 const postgres = require('../services/postgres');
 const logger = require('../utils/logger');
-const { verifyToken } = require('../utils/authToken');
+const { verifyToken, withToken } = require('../utils/authToken');
 
 module.exports = app =>
 	app.post('/verify-email', async (req, res) => {
 		const { token } = req.body;
 
 		try {
-			const user = verifyToken(token);
+			const { username } = verifyToken(token);
 
-			await postgres.query(
+			const { rows } = await postgres.query(
 				`
             update "user"
             set verified = true
-            where username = $1
+			where username = $1
+			returning 
+				username,
+				email,
+				password,
+				verified
         `,
-				[user.username]
+				[username]
 			);
 
-			res.send(user);
+			res.send(withToken(rows[0]));
 		} catch (error) {
 			logger.log(error);
 			res.status(400).send({});
